47ef0219330b3c06710dd85f86b882c7a9b76cea
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-09-12 00:46:53 +0300

    ... slider, adaptive

---
+++ js/ini.js
@@ -0,0 +1,65 @@
+$('.single-item').slick({
+  infinite: true,
+  // autoplay: true,
+  dots: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // adaptiveHeight:true,
+  responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 991.98,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 599.98,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
+$('.single-item_4').slick({
+  infinite: true,
+  autoplay: true,
+  pauseOnHover:false,
+  dots: false,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // adaptiveHeight:true,
+  responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow:3 ,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 425,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
