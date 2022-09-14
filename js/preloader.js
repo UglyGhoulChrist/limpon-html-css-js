3a037f0a452ce6839bc155321b7cdefec5fb9e83
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2022-09-14 16:26:00 +0300

    сделал правильно прелоадер

--- js/preloader.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    $preloader = $('.loader'),
-        $loader = $preloader.find('.demo');
-    $loader.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
